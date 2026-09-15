eca0ac04bec785ac4e716ec071776629187ea415
Alon Albert <aalbert@google.com>
2026-09-15 08:17:32 -0700

    Update Android SDK to 37

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,12 +16,12 @@ room3 {
 
 android {
   namespace = "com.alonalbert.enphase.monitor"
-  compileSdk = 36
+  compileSdk = 37
 
   defaultConfig {
     applicationId = "com.alonalbert.enphase.monitor"
     minSdk = 36
-    targetSdk = 36
+    targetSdk = 37
     versionCode = 1
     versionName = "1.0"
 
